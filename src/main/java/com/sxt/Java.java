3bc152fc3428bda444508c6decd3d456680a8a1b
hrz <497851031@qq.com>
2017-12-19 19:09:59 +0800

    "第一版"

---
+++ src/main/java/com/sxt/Java.java
@@ -0,0 +1,12 @@
+package com.sxt;
+
+import sun.applet.Main;
+
+/**
+ * Created by HRZ on 2017/12/19.
+ */
+public class Java {
+    public static void main(String []args){
+        System.out.print("hello");
+    }
+}
